839cec8fbd0ce9e5478dc59ad6894df77d7fd7dd
Yossi Rubinstein <59832349+yossiDisk@users.noreply.github.com>
2023-10-03 21:16:42 +0300

    Add files via upload

---
+++ json.js
@@ -0,0 +1,330 @@
+const compereData = [
+      {
+        "link": ["https://www.bestore.co.il/*"],
+        "name" : "BE פארם"
+
+      },
+      {
+        "link": ["https://glam42.co.il/?ref=Shamze"],
+        "name" : "GLAM42"
+
+      },
+      {
+        "link": ["https://track.wesell.co.il/click/SQg3190VCM05tfx/B9LBwvEVBst7kfk/TsSQg3190VCM05tfxtS"],
+        "name" : "סופר פארם"
+
+      },
+      {
+        "link": ["https://toysrus.co.il/"],
+        "name" : "טויס אר אס"
+
+      },
+      {
+        "link": ["https://www.kravitz.co.il/"],
+        "name" : "קרביץ"
+
+      },
+      {
+        "link": ["https://www.officedepot.co.il/"],
+        "name" : "אופיס דיפו"
+
+      },
+      {
+        "link": ["https://www.bug.co.il/"],
+        "name" : "באג"
+
+      },
+      {
+        "link": ["https://track.wesell.co.il/click/SQg3190VCM05tfx/606NlTaUduxgYgw/TsSQg3190VCM05tfxtS"],
+        "name" : "חשמל נטו"
+
+      },
+      {
+        "link": ["https://www.payngo.co.il/"],
+        "name" : "מחסני חשמל"
+
+      },
+      {
+        "link": ["https://track.wesell.co.il/click/SQg3190VCM05tfx/3BkSFv1D63FP80R/TsSQg3190VCM05tfxtS"],
+        "name" : "P1000"
+
+      },
+      {
+        "link": ["https://track.wesell.co.il/click/SQg3190VCM05tfx/a2WD4J1gE8Che1y/TsSQg3190VCM05tfxtS"],
+        "name" : "עולם הקולנוע"
+
+      },
+      {
+        "link": ["https://www.shekem-electric.co.il/"],
+        "name" : "שקם אלקטריק"
+
+      },
+      {
+        "link": ["https://track.wesell.co.il/click/SQg3190VCM05tfx/pbF7HYtvNATrsmY/TsSQg3190VCM05tfxtS"],
+        "name" : "א.ל.מ"
+
+      },
+      {
+        "link": ["https://track.clickon.co.il/click/fsEMzhDrS5aVVeV/ph0vD7VLdfyKvza/TsfsEMzhDrS5aVVeVtS"],
+        "name" : "אייבורי"
+
+      },
+      {
+        "link": ["https://track.wesell.co.il/click/SQg3190VCM05tfx/EJeNjS7u6ASlH9k/TsSQg3190VCM05tfxtS"],
+        "name" : "דינמיקה"
+
+      },
+      {
+        "ç": ["https://www.idigital.co.il/"],
+        "name" : "iDigital"
+
+      },
+      {
+        "link": ["https://ksp.co.il/item/F15500AX&homepage&param=SZZY"],
+        "name" : "KSP"
+      },
+    ]
+
+
+    const kuponData = 
+      [
+      {
+          "site": "המשביר 365",
+          "KUPON": "TSF8",
+          "KUPON_DEATAILS": "6% הנחה, לא כולל חשמל, קוסמטיקה ומזוודות. ללא כפל מבצעים וקופונים.\nבתוקף עד: 30.09.23",
+          "LINK": "https://track.wesell.co.il/click/SQg3190VCM05tfx/3l9Tb7u1Qj4uq8B/TsSQg3190VCM05tfxtS",
+          "URL": "https://365mashbir.co.il"
+      },
+      {
+          "site": "Twenty Four Seven",
+          "KUPON": "WER_TFS",
+          "KUPON_DEATAILS": "10% הנחה, ללא כפל מבצעים והטבות, רק על קולקציה חדשה, בלעדי לאתר",
+          "LINK": "https://track.clickon.co.il/click/fsEMzhDrS5aVVeV/ll78YfEblgR5Lzv/TsfsEMzhDrS5aVVeVtS",
+          "URL": "https://www.twentyfourseven.co.il"
+      },
+      {
+          "site": "רנואר",
+          "KUPON": "RENUAR-10",
+          "KUPON_DEATAILS": "קופון שמקנה 10% ללא כפל מבצעים, מימוש אחד ללקוח.\nללא תוקף כרגע",
+          "LINK": "https://track.clickon.co.il/click/fsEMzhDrS5aVVeV/TX98d7vuggTClVt/TsfsEMzhDrS5aVVeVtS",
+          "URL": "https://www.renuar.co.il"
+      },
+      {
+          "site": "WeShoes",
+          "KUPON": "clickon10",
+          "KUPON_DEATAILS": "אקסטרא 10% הנחה עם כפל הנחות עד 20%, לא כולל דגמי עודפים. ללא תוקף כרגע",
+          "LINK": "https://track.clickon.co.il/click/fsEMzhDrS5aVVeV/ep3EkhC7Xv1YAZn/TsfsEMzhDrS5aVVeVtS",
+          "URL": "https://weshoes.co.il"
+        },
+      {
+        "site": "פולגת - GOLF",
+        "KUPON": "FORYOU",
+        "KUPON_DEATAILS": "15% הנחה על כל האתר",
+        "LINK": "https://track.wesell.co.il/click/SQg3190VCM05tfx/FxQCx2S17Jlppy0/TsSQg3190VCM05tfxtS",
+        "URL": "https://www.polgat-il.co.il"
+    },
+      {
+          "site": "Ace",
+          "KUPON": "1100560",
+          "KUPON_DEATAILS": "5% הנחה על כל האתר ללא החרגות, ללא תוקף כרגע",
+          "LINK": "https://track.wesell.co.il/click/SQg3190VCM05tfx/JJSYcVbCmHqdgwz/TsSQg3190VCM05tfxtS",
+          "URL": "https://www.ace.co.il"
+      },
+      {
+          "site": "GADE",
+          "KUPON": "Wesell",
+          "KUPON_DEATAILS": "30% הנחה לא כולל מוצרים שבמבצע.\n ללא תוקף כרגע",
+          "LINK": "https://track.clickon.co.il/click/fsEMzhDrS5aVVeV/CC5Wj534hFRK81r/TsfsEMzhDrS5aVVeVtS",
+          "URL": "https://www.gade.co.il"
+      },
+      {
+          "site": "last minute - תיירות",
+          "KUPON": "lmpink23",
+          "KUPON_DEATAILS": "20$ הנחה בהזמנה מעל 400$. בתוקף עד 30.9.23",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "SOHO",
+          "KUPON": "SOHO20",
+          "KUPON_DEATAILS": "20% הנחה ללא כפל מבצעים והנחות. ללא תוקף כרגע",
+          "LINK": "https://track.wesell.co.il/click/SQg3190VCM05tfx/SlGEkEXKj2BwV04/TsSQg3190VCM05tfxtS",
+          "URL": "https://www.sohocenter.co.il"
+      },
+      {
+          "site": "ירין שחף - מכירות",
+          "KUPON": "YARIN10",
+          "KUPON_DEATAILS": "מקנה 10% הנחה על כל סל הקניות. ללא תוקף כרגע",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "Soltam",
+          "KUPON": "Soltam10",
+          "KUPON_DEATAILS": "מקנה 10% *כולל כפל מבצעים, על המגוון המשתתף במבצע. בתוקף עד 31.07.2023",
+          "LINK": "https://track.wesell.co.il/click/SQg3190VCM05tfx/sBG1U7Amz8Bv2ql/TsSQg3190VCM05tfxtS",
+          "URL": "https://soltam.co.il"
+      },
+      {
+          "site": "Terminal X",
+          "KUPON": "10p4terminal",
+          "KUPON_DEATAILS": "מקנה 10% כולל כפל רק למותגי הבית:\n1. טרמינל ילדים\n2. טרמינל נשים\n3. טרמינל גברים\n4. QM\n5. CUT N BASE\n6. אסנשילאס טרמינל\nתוקף: עד 31.12.23",
+          "LINK": "https://track.wesell.co.il/click/SQg3190VCM05tfx/lWe3N4x5dBDXbWU/TsSQg3190VCM05tfxtS",
+          "URL": "https://www.terminalx.com"
+      },
+      {
+          "site": "MiMas",
+          "KUPON": "MC15",
+          "KUPON_DEATAILS": "15% הנחה על כל האתר. ללא תוקף כרגע",
+          "LINK": "https://track.wesell.co.il/click/SQg3190VCM05tfx/JMg5MPuqNyk5usq/TsSQg3190VCM05tfxtS",
+          "URL": "https://www.mimas.co.il"
+      },
+      {
+          "site": "SABON",
+          "KUPON": "JUSTFREE",
+          "KUPON_DEATAILS": "משלוח חינם מהזמנה של 50 שח\nבין התאריכים: 10.09.2023-31.10.2023",
+          "LINK": "https://track.wesell.co.il/click/SQg3190VCM05tfx/39wYmLzEpChkkJD/TsSQg3190VCM05tfxtS",
+          "URL": "https://www.sabon.co.il"
+      },
+      {
+          "site": "Last Price",
+          "KUPON": "LPWE5",
+          "KUPON_DEATAILS": "5% הנחה על הקטגוריות הבאות: ריהוט, בישום, צעצועים. בתוקף עד ה-30.06.23",
+          "LINK": "https://track.wesell.co.il/click/SQg3190VCM05tfx/NVT7VkcPrRH5EYN/TsSQg3190VCM05tfxtS",
+          "URL": "https://www.lastprice.co.il"
+      },
+      {
+          "site": "שני פלצ'י",
+          "KUPON": "",
+          "KUPON_DEATAILS": "",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "ModiBodi",
+          "KUPON": "",
+          "KUPON_DEATAILS": "",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "Ahava",
+          "KUPON": "אהבה 25",
+          "KUPON_DEATAILS": "25% הנחה על ההזמנה הראשונה בלבד. ללא תוקף כרגע",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "Cook&Bake",
+          "KUPON": "cook10",
+          "KUPON_DEATAILS": "10% כולל כפל מבצעים. בתוקף עד ה-12.07.23",
+          "LINK": "https://track.wesell.co.il/click/SQg3190VCM05tfx/AvktpuyG0NanuDL/TsSQg3190VCM05tfxtS",
+          "URL": "https://www.cook-bake.co.il"
+      },
+      {
+          "site": "PerfectKitchen",
+          "KUPON": "Kupon5",
+          "KUPON_DEATAILS": "5% הנחה. ללא תוקף כרגע",
+          "LINK": "https://track.wesell.co.il/click/SQg3190VCM05tfx/2bY3dt9QkW0xqck/TsSQg3190VCM05tfxtS",
+          "URL": "https://perfectkitchen.co.il"
+      },
+      {
+          "site": "MediLink",
+          "KUPON": "medi5",
+          "KUPON_DEATAILS": "מקנה 5% הנחה על המוצרים בלינק medi במערכת.\nכולל כפל מבצעים אך לא כולל כפל קופונים\nתוקף: עד ה-31.08.23",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "DealCosmetics",
+          "KUPON": "",
+          "KUPON_DEATAILS": "",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "Papaya",
+          "KUPON": "",
+          "KUPON_DEATAILS": "",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "TouchTherapy",
+          "KUPON": "",
+          "KUPON_DEATAILS": "",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "Salt Water",
+          "KUPON": "",
+          "KUPON_DEATAILS": "",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "Afrodita",
+          "KUPON": "WELCOME",
+          "KUPON_DEATAILS": " פעיל החל מה-27.9- 10% הנחה על האתר, כולל כפל מבצעים. \nחשוב! החרגות: הקופון בתוקף עד 31.10.23, מימוש 1 ללקוח, ההטבה באתר בלבד, אקסטרה הנחה תינתן על היתרה, על הפריטים המשתתפים במבצע בלבד, אין כפל קופונים. \n ",
+          "LINK": "https://track.wesell.co.il/click/SQg3190VCM05tfx/VkC7cXwsHSWG9gw/TsSQg3190VCM05tfxtS",
+          "URL": "https://www.afrodita.co.il"
+      },
+      {
+          "site": "חשמל נטו",
+          "KUPON": "",
+          "KUPON_DEATAILS": "",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "MediLink",
+          "KUPON": "medi5",
+          "KUPON_DEATAILS": "מקנה 5% הנחה על המוצרים בלינק medi במערכת.\nכולל כפל מבצעים אך לא כולל כפל קופונים\nתוקף: עד ה-31.08.23",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "DealCosmetics",
+          "KUPON": "",
+          "KUPON_DEATAILS": "",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "Papaya",
+          "KUPON": "",
+          "KUPON_DEATAILS": "",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "TouchTherapy",
+          "KUPON": "",
+          "KUPON_DEATAILS": "",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "Salt Water",
+          "KUPON": "",
+          "KUPON_DEATAILS": "",
+          "LINK": "",
+          "URL": ""
+      },
+      {
+          "site": "DYN Store",
+          "KUPON": "dynweboom",
+          "KUPON_DEATAILS": "15 שח בקנייה מעל 250 שח, בתוקף עד ל31.12.23",
+          "LINK": "https://track.wesell.co.il/click/SQg3190VCM05tfxt/sBG1U7Amz8Bv2ql/TsSQg3190VCM05tfxtS",
+          "URL": ""
+      },
+      {
+          "site": "חשמל נטו",
+          "KUPON": "",
+          "KUPON_DEATAILS": "",
+          "LINK": "",
+          "URL": ""
+      }
+    ]
+  
---
+++ script.js
@@ -0,0 +1,45 @@
+const compareButton = document.getElementById('compareButton');
+const couponButton = document.getElementById('couponButton');
+const content = document.getElementById('content');
+
+let currentData = [];
+
+compareButton.addEventListener('click', () => {
+    compareButton.classList.add('selected');
+    couponButton.classList.remove('selected');
+    generateButtons(kuponData); // Use kuponData from json.js
+});
+
+couponButton.addEventListener('click', () => {
+    couponButton.classList.add('selected');
+    compareButton.classList.remove('selected');
+    generateButtons(compereData); // Use compereData from json.js
+});
+
+function generateButtons(data) {
+    content.innerHTML = ''; // Clear existing buttons
+
+    data.forEach((item) => {
+        // Check if the URL is not an empty string
+        if (item.URL || (item.link && item.link[0])) {
+            const buttonContainer = document.createElement('div');
+            buttonContainer.className = 'button-container';
+
+            const buttonLink = document.createElement('a');
+            buttonLink.href = item.LINK || item.link[0];
+            buttonLink.target = '_blank'; // Open in a new tab
+            buttonLink.className = 'button-link';
+
+            const button = document.createElement('button');
+            button.textContent = item.site || item.name;
+            button.className = 'button';
+
+            buttonLink.appendChild(button);
+            buttonContainer.appendChild(buttonLink);
+            content.appendChild(buttonContainer);
+        }
+    });
+}
+
+// Initialize with the default data (השוואה list) when the page loads
+generateButtons(kuponData); // Use kuponData from json.js
